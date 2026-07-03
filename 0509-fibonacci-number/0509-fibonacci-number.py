8517627c87bfb740084c8a96fcac4e208af03106
Vaikunth Prajapati <137423457+vaikunthprajapati@users.noreply.github.com>
2026-07-03 18:31:28 +0530

    Time: 631 ms (30.32%), Space: 12.4 MB (16.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,5 @@
-class Solution:
+class Solution(object):
     def fib(self, n):
-        memo = {}
-
-        def dfs(n):
-            if n <= 1:
-                return n
-
-            if n in memo:
-                return memo[n]
-
-            memo[n] = dfs(n - 1) + dfs(n - 2)
-            return memo[n]
-
-        return dfs(n)
+        if (n<=1):
+            return n
+        return self.fib(n-1) + self.fib(n-2)
