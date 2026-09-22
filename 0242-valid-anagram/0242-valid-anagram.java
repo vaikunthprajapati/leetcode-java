ce10e5e869d94c8a1b7608545a05a302507fa1dd
Vaikunth Prajapati <137423457+vaikunthprajapati@users.noreply.github.com>
2026-09-22 11:09:15 +0530

    Time: 5 ms (63.1%), Space: 44.1 MB (94.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,22 +3,16 @@ class Solution {
         if(s.length() != t.length()){
         return false;
     }
-    HashMap<Character,Integer> map = new HashMap<>();
-    for(char c: s.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
-        }
-    for(char c: t.toCharArray()){
-        if(!map.containsKey(c)){
+    int[] count = new int[256];
+    for(int i =0; i<s.length(); i++){
+        count[s.charAt(i)]++;
+        count[t.charAt(i)]--;
+    }
+    for(int n: count){
+        if(n != 0){
             return false;
         }
-        else{
-            map.put(c, map.get(c)-1);
-        }
-        if(map.get(c) == 0){
-            map.remove(c);
-        }
     }
-    return map.isEmpty();
-
-    }
+    return true;
+}
 }
