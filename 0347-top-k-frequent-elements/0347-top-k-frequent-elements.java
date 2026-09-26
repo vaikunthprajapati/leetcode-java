d79e68a0f850624cbf4cdb22e41bd61675a88988
Vaikunth Prajapati <137423457+vaikunthprajapati@users.noreply.github.com>
2026-09-26 18:26:15 +0530

    Time: 16 ms (39.68%), Space: 47.3 MB (95.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -9,7 +9,7 @@ class Solution {
         map.put(num,map.getOrDefault(num,0)+1);
        }
      }
-     PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+     PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
      for(Map.Entry<Integer,Integer> entry: map.entrySet()){
         heap.offer(new int[]{entry.getKey(), entry.getValue()});
         if(heap.size()>k){
